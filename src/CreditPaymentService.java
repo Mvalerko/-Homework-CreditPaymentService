36c7f89dd4d6a8585875b15e7bf33bc85d56487e
Mikhail Valerko <mixelv@mail.ru>
2023-02-02 13:04:58 +0100

    Добавлены коментарии коду

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,11 @@
 public class CreditPaymentService {
 
-
+    // Метод calculate выполняет только расчет ежемесячного платежа исходя из данных о сумме займа, процентной ставке
+    // и сроке займа по формуле, представленой
+    // на сайте банка https://sovcombank.ru/blog/krediti/kak-rasschitat-annuitetnii-platezh
     public double calculate (int amount, double percent, int term) {
 
-
+        // расчет, который происходит дальше сложно объяснить без иллюстраций :) Обращайтесь к автору
         double result;
         double pM = percent / 100;
         double cof = pM / 12;
@@ -15,6 +17,8 @@ public class CreditPaymentService {
         double stepOne = amount * cof * multiplierM;
 
         double stepTwo = stepOne / divider;
+
+
 
         return result = stepTwo;
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        // Создаем переменную типа CreditPaymentService и указываем, что ее значением будет класс CreditPaymentService
         CreditPaymentService service = new CreditPaymentService();
-        int amount = 1_000_000;
-        double percent = 9.99;
-        int term = 36;
-        double payment = service.calculate(amount, percent, term);
-        System.out.printf("%.3f", payment);
+        /*
+        Отладочные данные
+
+        int amount = 2_367_000;
+        double percent = 9.1;
+        int term = 240;
+        */
+
+        //Вызываем метод calculate класса CreditPaymentService и сразу указываем параметры переменных, которые обозначены внутри CreditPaymentService
+        System.out.println ("При оформлении 1 миллиона рублей в займ под 9.99% сроком на один год ");
+        System.out.println ("Сумма ежемесячного платежа составит " + Math.round(service.calculate(1_000_000, 9.99, 12)) + " руб.");
+        System.out.println ("При оформлении 1 миллиона рублей в займ под 9.99% сроком на два года ");
+        System.out.println ("Сумма ежемесячного платежа составит " + Math.round(service.calculate(1_000_000, 9.99, 24)) + " руб.");
+        System.out.println ("При оформлении 1 миллиона рублей в займ под 9.99% сроком на три года ");
+        System.out.println ("Сумма ежемесячного платежа составит " + Math.round(service.calculate(1_000_000, 9.99, 36)) + " руб.");
     }
 }
